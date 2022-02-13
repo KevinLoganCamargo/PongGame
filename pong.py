8d361da14831bba85c8a2b0d1daf9f26ed787ccd
Logan <98756201+KevinLoganCamargo@users.noreply.github.com>
2022-02-13 11:48:45 -0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,16 +1,9 @@
 from tkinter import *
 import random
 import time
-import win32gui,win32con
-
-try:
-    hide_window=win32gui.GetForegroundWindow()
-    win32gui.ShowWindow(hide_window, win32con.SW_HIDE)
-except:
-    pass
 
 level=int(input('Digite em que nível deseja jogar 1/2/3/4/5\n'))
-length=500/level
+length=400/level
 
 root=Tk()
 root.title('PONG')
@@ -22,17 +15,16 @@ canvas.pack()
 
 root.update()
 
-# Variável
 count = 0
 lost = False
 
 
 class Ball:
-    def __init__(self, canvas, Barra, color):
+    def __init__(self, canvas, Bar, color):
         self.canvas = canvas
-        self.Barra = Barra
+        self.Bar = Bar
         self.id = canvas.create_oval(0, 0, 15, 15, fill=color)
-        self.canvas.move(self.id, 20/level, 200)
+        self.canvas.move(self.id, 200, 200)
 
         starts_x = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts_x)
@@ -72,10 +64,10 @@ class Ball:
             elif level==3 or level==4 or level==5:
                 self.x = -2*level
 
-        self.Barra_pos = self.canvas.coords(self.Barra.id)
+        self.Bar_pos = self.canvas.coords(self.Bar.id)
 
-        if pos[2] >= self.Barra_pos[0] and pos[0] <= self.Barra_pos[2]:
-            if pos[3] >= self.Barra_pos[1] and pos[3] <= self.Barra_pos[3]:
+        if pos[2] >= self.Barra_pos[0] and pos[0] <= self.Bar_pos[2]:
+            if pos[3] >= self.Barra_pos[1] and pos[3] <= self.Bar_pos[3]:
                 if level == 1 or level == 2:
                     self.y = -3 * level
                 elif level == 3 or level == 4 or level == 5:
@@ -144,7 +136,7 @@ def start_game(event):
     canvas.itemconfig(game, text=" ")
 
     time.sleep(1)
-    Barra.draw()
+    Bar.draw()
     Ball.draw()
 
 
@@ -155,7 +147,7 @@ def score():
 def game_over():
     canvas.itemconfig(game, text="Game over!")
 
-Barra = Barra(canvas, "blue")
+Bar = Bar(canvas, "blue")
 Ball = Ball(canvas, Barra, "red")
 
 score_now = canvas.create_text(430, 20, text="Pontos: " + str(count), fill="green", font=("Arial", 16))
